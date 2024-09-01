68f6f7a7d35c07f2d8c6e74b67d5cd6e0d8c3e55
woojin <ujinjeong55@gmail.com>
2024-09-01 14:35:28 +0900

    [add] Lesson 42
    invisible container pushes sibling containers all the way to the right side

--- lib/main.dart
+++ lib/main.dart
@@ -6,15 +6,33 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Container(
-            height: 100.0,
-            width: 100.0,
-            margin: EdgeInsets.only(left: 30.0),
-            padding: EdgeInsets.all(20.0),
-            color: Colors.white,
-            child: Text('Hello'),
-          ),
-        ),
+            child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Container(
+              height: 100.0,
+              width: 100.0,
+              color: Colors.white,
+              child: Text('Container 1'),
+            ),
+            Container(
+              height: 100.0,
+              width: 100.0,
+              color: Colors.blue,
+              child: Text('Container 2'),
+            ),
+            Container(
+              height: 100.0,
+              width: 100.0,
+              color: Colors.red,
+              child: Text('Container 3'),
+            ),
+            Container(
+              width: double.infinity,
+              height: 10.0,
+            )
+          ],
+        )),
       ),
     ),
   );
