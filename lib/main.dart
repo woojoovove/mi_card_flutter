b1cb71d8f7fcc9b6ac671c58f6c69cf17b77d103
woojin <ujinjeong55@gmail.com>
2024-09-01 14:24:49 +0900

    [add] Lesson 41

--- lib/main.dart
+++ lib/main.dart
@@ -7,8 +7,12 @@ void main() {
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Container(
+            height: 100.0,
+            width: 100.0,
+            margin: EdgeInsets.only(left: 30.0),
+            padding: EdgeInsets.all(20.0),
             color: Colors.white,
-            child: Text('Hello Hi'),
+            child: Text('Hello'),
           ),
         ),
       ),
