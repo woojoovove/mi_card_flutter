829ac8deb77df48b7af1017b3230dd829cf137cb
woojin <ujinjeong55@gmail.com>
2024-09-08 14:13:06 +0900

    [add] Lesson 47 Use Card and ListTitle

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,41 @@ void main() {
                   color: Colors.white,
                   fontSize: 20.0,
                   letterSpacing: 2.5),
+            ),
+            Column(
+              children: [
+                Card(
+                    color: Colors.white,
+                    margin:
+                        EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.phone,
+                        color: Colors.teal,
+                      ),
+                      title: Text('+82 10 5038 8353',
+                          style: TextStyle(
+                            color: Colors.teal.shade900,
+                            fontFamily: 'SourceSans3',
+                            fontSize: 20.0,
+                          )),
+                    )),
+                Card(
+                    color: Colors.white,
+                    margin:
+                        EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                    child: ListTile(
+                        leading: Icon(
+                          Icons.mail,
+                          color: Colors.teal,
+                        ),
+                        title: Text('ujinjeong55@gmail.com',
+                            style: TextStyle(
+                              color: Colors.teal.shade900,
+                              fontFamily: 'SourceSans3',
+                              fontSize: 20.0,
+                            ))))
+              ],
             )
           ],
         ),
